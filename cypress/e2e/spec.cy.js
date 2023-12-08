7a1879c8a2174cc867c7201bda3abbf6ce0635e7
ANZHELIKA <angela.pikulina@gmail.com>
2023-12-08 22:08:25 +0100

    HW_5_1_2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,25 @@
+/// <reference types="cypress" />
+describe('Проверка элементов на странице', () => {
+  beforeEach(() => {
+    // Заходим на вашу страницу перед каждым тестом
+    cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com');
+  });
 
+  it('Должен содержать div с классом "app-container" и стилем padding-top: 60px', () => {
+    // Проверка наличия элемента с классом "app-container"
+    cy.get('.app-container').should('exist');
 
-describe('Проверка загрузки страницы', () => {
-  it('Страница должна загрузиться', () => {
-    cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com'); 
-    cy.get('body').should('exist');
-    cy.get('.app-container').should('be.visible');
-  
+    // Проверка стиля padding-top у элемента с классом "app-container"
+    cy.get('.app-container').should('have.css', 'padding-top', '60px');
+  });
+
+  it('Должен содержать элемент с классом "Toastify"', () => {
+    // Проверка наличия элемента с классом "Toastify"
+    cy.get('.Toastify').should('exist');
+  });
+
+  it('Должен содержать элемент с id "app-header"', () => {
+    // Проверка наличия элемента с id "app-header"
+    cy.get('#app-header').should('exist');
   });
 });
