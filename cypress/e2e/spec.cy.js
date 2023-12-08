e3e72400088879c6fbe4935d03da862c0ec2d94c
ANZHELIKA <angela.pikulina@gmail.com>
2023-12-08 22:15:36 +0100

    HW_5_1_2/2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('Проверка элементов на странице', () => {
   beforeEach(() => {
-    // Заходим на вашу страницу перед каждым тестом
+    // Заходим на эту страницу перед каждым тестом
     cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com');
   });
 
