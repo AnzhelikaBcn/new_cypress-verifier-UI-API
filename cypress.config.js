9f48c9cef057a55ec95edcca6ff3dc6fe44b9c88
ANZHELIKA <angela.pikulina@gmail.com>
2023-12-15 10:38:13 +0100

    HW-5-3

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   projectId: "2k5yog",
   e2e: {
     baseUrl: "https://example.cypress.io",
-    
+
     // измененные тесты не будут отражаться на доске автоматически
-    watchForFileChanges: false, 
-    
+    //watchForFileChanges: false,
+
     setupNodeEvents(on, config) {
       //
     },
--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,8 +1,25 @@
+const { before } = require("cypress/types/lodash");
+
 describe("First tests suit", () => {
+  before(() => {
+    cy.log("before hook executed");
+  });
+
   it("First test", () => {
     cy.visit("/todo");
     //cy.log("test is still going");
     cy.log("Current base url" + Cypress.config("baseUrl"));
     cy.get("h1").debug().should("be.visible");
+  });
+
+  it.only("Test for .then()", () => {
+    cy.visit("/todo");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get(".todo-list li").then((number) => {
+      expect(number.length).to.equal(5);
+      //assert.equal(number.length, 5, "Array number is right");
+    });
   });
 });
